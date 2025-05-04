00554896e7a670f9414a337ab7f00c9807bc791d
shakal2004 <bl4ckr0s3@mail.ru>
2025-05-04 18:17:48 +0300

    update task01, task02

---
+++ Task02/logic.cpp
@@ -0,0 +1,55 @@
+﻿// [The Sum of elements]
+// Сумма элементов
+//
+// Дан вектор целочисленных значений. Необходимо разработать
+// функцию(или программу), которая вычисляет сумму элементов
+// вектора, расположенных между первым и последним нулевыми
+// элементами. Если вектор содержит только один нулевой элемент
+// или не содержит вообще нулевых элементов, а также, если
+// вектор задан некорректно, то функция(программа) должна
+// возвратить ноль.
+
+#include "logic.h"
+
+int get_first_zero_index(int* array, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		if (*(array + i) == 0) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+
+
+
+int get_last_zero_index(int* array, int size) {
+	for (int i = size - 1; i >= 0; i--)
+	{
+		if (*(array + i) == 0) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int sum_elements(int* array, int size) {
+	if (size <= 2 || array == nullptr) {
+		return 0;
+	}
+
+	int sum = 0;
+
+	int first = get_first_zero_index(array, size);
+	int last = get_last_zero_index(array, size);
+
+	for (int i = first + 1; i < last; i++)
+	{
+		sum += *(array + i);
+	}
+
+	return sum;
+}
